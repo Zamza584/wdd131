9738d3f820b9b040fc350e2c5d2488a31a9c0334
Manuel Zamalloa LL <mzamalloa97@gmail.com>
2025-07-21 22:23:16 -0500

    lazy load

---
+++ week04/scripts/practice.js
@@ -0,0 +1,17 @@
+let aCourse = {
+  code: "WDD131",
+  title: "Dynamic Web Fundamentals",
+  credits: 2,
+  sections: [
+    {
+      section: "A",
+      enrolled: 43,
+      instructor: "James",
+    },
+    {
+      section: "B",
+      enrolled: 50,
+      instructor: "Paul",
+    },
+  ],
+};
